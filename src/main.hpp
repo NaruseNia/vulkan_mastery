fe9b30d28a6786c1ffa19a162e0468187d9be697
NaruseNia <notshinsin0000@gmail.com>
2024-10-21 01:40:13 +0900

    Setup project

---
+++ src/main.hpp
@@ -0,0 +1,11 @@
+#ifndef VULKAN_MASTERY_MAIN_H
+#define VULKAN_MASTERY_MAIN_H
+
+#define VOLK_IMPLEMENTATION
+#include "Volk/volk.h"
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image_resize2.h"
+
+#endif //VULKAN_MASTERY_MAIN_H
